bb1f44fb4a6cef427e8919ba3b554d8fe470460f
irus1234 <88254362+irus1234@users.noreply.github.com>
2025-10-09 00:06:00 -0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
 class Calculator:
-    def sumar(self, a, b): return a - b
+    def sumar(self, a, b): return a + b
     def restar(self, a, b): return a - b
     def multiplicar(self, a, b): return a * b
     def dividir(self, a, b):
